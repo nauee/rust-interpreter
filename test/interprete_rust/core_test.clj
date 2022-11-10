51c276f8037dd7cc1d4e0490c0881fc171d741ba
Nahuel Castro <68122322+nauee@users.noreply.github.com>
2022-11-10 16:46:08 -0300

    Init

---
+++ test/interprete_rust/core_test.clj
@@ -0,0 +1,7 @@
+(ns interprete-rust.core-test
+  (:require [clojure.test :refer :all]
+            [interprete-rust.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
